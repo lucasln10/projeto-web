950fb56826ff211a04f9db529e4a2edb45c9cfde
H. Lucas <151595379+lucasln10@users.noreply.github.com>
2025-09-26 00:07:34 +0000

    alterações

--- projeto-web/resources/views/login.blade.php
+++ projeto-web/resources/views/login.blade.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login</title>
+<link rel="stylesheet" href="../login.css">
 </head>
 <body>
-    <div class="form-login">
-        <h1>Login</h1>
-        <form method="POST" action="{{ route('login.post') }}">
-            @csrf
-            <label for="name">Nome:</label>
-            <input type="text" id="name" name="name" required>
-            <br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br>
-            <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" required>
-            <br>
-            <button type="submit">Entrar</button>
-        </form>
-        <div>
-            <p>Não tem uma conta? <a href="{{ route('register.view') }}">Registre-se aqui</a></p>
-        </div>
-    </div>
+<div class="login-container">
+<h1>Login</h1>
+<form method="POST" action="{{ route('login.post') }}">
+    @csrf
+    <label for="name">Nome:</label>
+    <input type="text" id="name" name="name" required>
+    <br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="password">Senha:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Entrar</button>
+</form>
+</div>
+<footer>
+<div>
+<p>Não tem uma conta? <a href="{{ route('register.view') }}">Registre-se aqui</a></p>
+</div>
+</footer>
 </body>
 </html>
